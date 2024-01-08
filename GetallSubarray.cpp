ff2bc3b6c202d4ea15adf0a6fa9d33d4163d26b8
ShivamShah12 <112316509+Shivam-shah12@users.noreply.github.com>
2024-01-08 00:53:33 -0800

    Update GetallSubarray.cpp

--- GetallSubarray.cpp
+++ GetallSubarray.cpp
@@ -4,10 +4,10 @@ vector<vector<int>> getSubarray(vector<int>arr)
   int n=arr.size();
   for(int i=0;i<n;i++)
   {
-    for(int j=0;j<=i;j++)
+    for(int j=0;j<n;j++)
     {
       vector<int>temp;
-      for(int k=j;k<=i;k++)
+      for(int k=i;k<=j;k++)
       {
         temp.push_back(arr[k]);
       }
