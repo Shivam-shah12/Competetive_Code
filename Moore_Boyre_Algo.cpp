5c572571d9121f37a7b3a8cc4757904089ef6ee1
ShivamShah12 <112316509+Shivam-shah12@users.noreply.github.com>
2024-03-09 07:03:11 -0800

    Create Moore_Boyre_Algo.cpp

---
+++ Moore_Boyre_Algo.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Majority(vector<int>&nums,int size)
+{
+  int count=1;
+  int curr_major_ind=0;
+  int ind=1;
+  while(ind<size)
+  {
+    if(nums[curr_major_ind]==nums[ind])
+    {
+      count++;
+      ind++;
+    }
+    else
+    {
+      // agar curr->index wala element , Majority element 
+      // nahi toh iska mtlb vo ek space kha raha jha par majority element
+       // hona chahiye
+      count--;
+    }
+    if(count==0)
+    {
+      // yadi mera count 0 hai 
+      // iska mtlb me jisko majority mnn raha tha uske 
+      // jitne alg alg candidate bhi toh iska mtlb vo
+      // majority nhi ho skta
+      // --> then choose new Majority element
+      curr_major_ind=ind;
+      ind++;
+    }
+  }
+  int cnt=0;
+  for(int i=0;i<size;i++)
+  {
+    if(nums[i]==nums[curr_major_ind])
+      cnt++;
+  }
+  return cnt;
+}
+
+int main()
+{
+  init_code();
+  int n;
+  cout<<"Enter the length of Array ::  ";
+  cin>>n;
+  cout<<n<<endl;
+  vector<int>nums(n);
+  for(int i=0;i<n;i++)
+  {
+    cin>>nums[i];
+  }
+  cout<<Majority(nums,n);
+  
+ 
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
